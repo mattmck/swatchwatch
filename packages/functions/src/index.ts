b94573fdda29ec633af402c013d57be98fe45679
Test User <test@example.com>
2026-02-12 04:15:52 -0500

    fix(functions): Consolidate function exports to index.ts and update package.json

---
+++ packages/functions/src/index.ts
@@ -0,0 +1,7 @@
+export * from './functions/auth';
+export * from './functions/capture';
+export * from './functions/catalog';
+export * from './functions/ingestion-worker';
+export * from './functions/ingestion';
+export * from './functions/polishes';
+export * from './functions/voice';
